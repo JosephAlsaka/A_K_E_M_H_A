aac0f36ab86c0fd84f5b5e069ae54bbbabe4682c
Sami <sami.amin.6.2001@gmail.com>
2024-03-30 22:51:26 +0300

    first commit

---
+++ src/main/java/com/grad/akemha/AkemhaApplication.java
@@ -0,0 +1,13 @@
+package com.grad.akemha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkemhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkemhaApplication.class, args);
+	}
+
+}

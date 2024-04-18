b65f0e89bc7ad209a27e1984367b3a82b9f1d7d8
Sami <sami.amin.6.2001@gmail.com>
2024-04-18 21:56:03 +0300

    test

--- src/main/java/com/grad/akemha/entity/User.java
+++ src/main/java/com/grad/akemha/entity/User.java
@@ -5,7 +5,6 @@ import com.grad.akemha.entity.enums.Gender;
 import com.grad.akemha.entity.enums.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -54,24 +52,20 @@ public class User implements UserDetails {
     @Column(name = "dob", nullable = true)
     private LocalDate dob;
 
-    @Column(name = "profile_image",nullable = true)
+    @Column(name = "profile_image", nullable = true)
     private String profileImage;
 
-    @Column(name = "is_active",nullable = true)
+    @Column(name = "is_active", nullable = true)
     private Boolean isActive = true;
 
-    @Column(name = "description",nullable = true)
+    @Column(name = "description", nullable = true)
     private String description;
 
-@Enumerated(EnumType.STRING)
-private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
-    @Column(name = "location",nullable = true)
+    @Column(name = "location", nullable = true)
     private String location;
-
-    @Column(name = "location",nullable = true)
-    private String location;
-
 
     @JsonIgnore
     @ManyToOne
@@ -91,8 +85,8 @@ private Gender gender;
 //        return null;
         List<GrantedAuthority> list = new ArrayList<GrantedAuthority>();
         String role = String.valueOf(getRole());
-        list.add(new SimpleGrantedAuthority("ROLE_"+role));
-        System.out.println("list is"+list);
+        list.add(new SimpleGrantedAuthority("ROLE_" + role));
+        System.out.println("list is" + list);
         return list;
     }
 
@@ -110,6 +104,7 @@ private Gender gender;
     public boolean isAccountNonLocked() {
         return true;
     }
+
     @Override
     public boolean isCredentialsNonExpired() {
         return true;

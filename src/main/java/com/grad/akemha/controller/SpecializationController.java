03ade5c81e730351527a15d1db658012c2393936
Joseph Alsaka <joseph70.alsaka@gmail.com>
2024-04-18 12:26:29 +0300

    add @CrossOrigin to SpecializationController

--- src/main/java/com/grad/akemha/controller/SpecializationController.java
+++ src/main/java/com/grad/akemha/controller/SpecializationController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/specialization")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SpecializationController {
 
     @Autowired

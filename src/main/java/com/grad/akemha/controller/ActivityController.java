b409dc7a828f658e912d0ae3b409d068ac410491
Joseph Alsaka <joseph70.alsaka@gmail.com>
2024-06-17 20:55:23 +0300

    add @CrossOrigin to ActivityController

--- src/main/java/com/grad/akemha/controller/ActivityController.java
+++ src/main/java/com/grad/akemha/controller/ActivityController.java
@@ -21,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/activity")
 @RequiredArgsConstructor
 public class ActivityController {

647ff798bcc0202bf0e32d7e0dba2c57c1675ae1
Joseph Alsaka <joseph70.alsaka@gmail.com>
2024-06-24 21:15:39 +0300

    return null and Taken status in user reservation

--- src/main/java/com/grad/akemha/controller/MedicalDeviceController.java
+++ src/main/java/com/grad/akemha/controller/MedicalDeviceController.java
@@ -3,7 +3,9 @@ package com.grad.akemha.controller;
 import com.grad.akemha.dto.BaseResponse;
 import com.grad.akemha.dto.medicalDevice.AddDeviceRequest;
 import com.grad.akemha.dto.medicalDevice.ChangeQuantityRequest;
+import com.grad.akemha.dto.medicalDevice.ReservationResponse;
 import com.grad.akemha.dto.medicalDevice.ReserveDeviceRequest;
+import com.grad.akemha.dto.post.PostResponse;
 import com.grad.akemha.entity.DeviceReservation;
 import com.grad.akemha.entity.MedicalDevice;
 import com.grad.akemha.service.MedicalDeviceService;
@@ -59,8 +61,10 @@ public class MedicalDeviceController {
 
 
     @GetMapping("/user")
-    public ResponseEntity<BaseResponse<List<DeviceReservation>>> getUserReservations(@RequestHeader HttpHeaders httpHeaders) {
-        return ResponseEntity.ok().body(new BaseResponse<>(HttpStatus.OK.value(), " user device reservations", medicalDeviceService.getUserReservations(httpHeaders)));
+    public ResponseEntity<BaseResponse<List<ReservationResponse>>> getUserReservations(@RequestHeader HttpHeaders httpHeaders) {
+        List<DeviceReservation> reservations=medicalDeviceService.getUserReservations(httpHeaders);
+        List<ReservationResponse> response = reservations.stream().map(ReservationResponse::new).toList();
+        return ResponseEntity.ok().body(new BaseResponse<>(HttpStatus.OK.value(), " user device reservations", response));
     }
 
     @PostMapping("reserve")
--- src/main/java/com/grad/akemha/repository/DeviceReservationRepository.java
+++ src/main/java/com/grad/akemha/repository/DeviceReservationRepository.java
@@ -4,6 +4,8 @@ import com.grad.akemha.entity.DeviceReservation;
 import com.grad.akemha.entity.User;
 import com.grad.akemha.entity.enums.DeviceReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,7 +17,11 @@ public interface DeviceReservationRepository extends JpaRepository<DeviceReserva
 
     List<DeviceReservation> findByExpirationTimeBeforeAndStatusIsNull(LocalDateTime currentTime);
 
-    List<DeviceReservation> findByUserAndStatusIn(User user, List<DeviceReservationStatus> statuses);
+//    List<DeviceReservation> findByUserAndStatusIn(User user, List<DeviceReservationStatus> statuses);
+
+    @Query("SELECT dr FROM DeviceReservation dr WHERE dr.user = :user AND (dr.status IN :statuses OR dr.status IS NULL) ORDER BY dr.id DESC")
+    List<DeviceReservation> findByUserAndStatusIn(@Param("user") User user, @Param("statuses") List<DeviceReservationStatus> statuses);
+
 
 
 
